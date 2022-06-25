b27c66a711c444cb814a4ac6555ceae98c2f027e
EgorVyhodcev <wf-game-acc@bk.ru>
2022-06-25 16:34:09 +0300

    Ver 3.0

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import cv2
 import pickle
-import cvzone
-import numpy as np
+from PIL import Image
+from cvzone import putTextRect
+import os
 
 """
 Teeth AI
 """
 check_list = []
-hazard_list = []
 
 
 def check_karies(processed_img, flag):
-    controversial = False
-    print(pos_list)
     for pos in pos_list:
         x, y = pos
         crop_img = processed_img[y: y + height, x: x + width]
-        cv2.imshow(str(x * y) + flag, crop_img)
         count = cv2.countNonZero(crop_img)
-        cvzone.putTextRect(
-            img, str(count), (x, y + height - 3), scale=1, thickness=1,
-            offset=0
-        )
 
         if flag == 'rgb':
             if count > 100:
@@ -31,28 +24,18 @@ def check_karies(processed_img, flag):
                 color = (0, 255, 0)
         else:
             if count > 83:
-                #hazard_list.append(pos)
                 color = (0, 0, 255)
             else:
                 color = (0, 255, 0)
         if 0 < len(check_list) < 2 and flag == 'hsv' and pos == check_list[0]:
-            hazard_list.append(pos)
             color = (0, 0, 255)
             cv2.rectangle(
                 img, check_list[0], (check_list[0][0]+width,
                                      check_list[0][1]+height), color, 2
             )
-            """cvzone.putTextRect(
-                img, 'Karies', (x, y + height - 3), scale=1, thickness=1,
-                offset=0
-            )"""
-            controversial = True
         else:
             if color == (0, 0, 255):
-                """cvzone.putTextRect(
-                    img, 'Karies', (x, y + height - 3), scale=1, thickness=1,
-                    offset=0
-                )"""
+
                 cv2.rectangle(
                     img, pos, (pos[0] + width, pos[1] + height), color, 2
                 )
@@ -60,8 +43,6 @@ def check_karies(processed_img, flag):
                 cv2.rectangle(
                     img, pos, (pos[0] + width, pos[1] + height), color, 2
                 )
-
-    return controversial
 
 
 width, height = 73, 60
@@ -69,41 +50,52 @@ width, height = 73, 60
 with open('box_pos', 'rb') as f:
     pos_list = pickle.load(f)
 
-img_name = '1src.jpg'
+img_name = '6.jpg'
 
 img = cv2.imread(img_name)
 img = cv2.resize(img, (256, 256))
+putTextRect(img, '1', (2, 30), scale=3, thickness=2, offset=0)
 cv2.imshow("orig_img", img)
 cv2.waitKey(0)
-# img_crop = img[23:211, 20:230]
 low_black = (23, 22, 41)
 high_black = (35, 70, 100)
 processed_karies = cv2.inRange(img, low_black, high_black)
 cv2.imshow("processed in rgb!", processed_karies)
 
-contr_sit = check_karies(processed_karies, flag='rgb')
+check_karies(processed_karies, flag='rgb')
 
 cv2.imshow("img_after_mask", img)
-
+cv2.imwrite('rgb_res.jpg', img)
 img = cv2.imread(img_name)
 img = cv2.resize(img, (256, 256))
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 cv2.imshow("hsv", img_hsv)
-low_black = (4, 169, 40)
-high_black = (20, 215, 155)
-processed_hsv_karies = cv2.inRange(img_hsv, low_black, high_black)
+putTextRect(img_hsv, '3', (2, 30), scale=3, thickness=2, offset=0)
+cv2.imwrite('hsv_filter.jpg', img_hsv)
+hsv_low = (4, 169, 40)
+hsv_high = (20, 215, 155)
+processed_hsv_karies = cv2.inRange(img_hsv, hsv_low, hsv_high)
 cv2.imshow('processed in HSV!', processed_hsv_karies)
-contr_sit = check_karies(processed_hsv_karies, flag='hsv')
 
-if contr_sit:
-    print(f"СПОРНАЯ СИТУАЦИЯ, количество очагов: {len(hazard_list)}! ")
-    cv2.imshow('Controversial situation', img)
-else:
-    cv2.imshow(f'Discovered {len(hazard_list)} hazards!', img)
-    print(f"Количество очагов: {len(hazard_list)}!")
+check_karies(processed_hsv_karies, flag='hsv')
+
+putTextRect(img, '2', (2, 30), scale=3, thickness=2, offset=0)
+cv2.imwrite('hsv_res.jpg', img)
+cv2.imshow(f'Discovered hazards!', img)
+
+im_orig = Image.open(img_name)
+im1 = Image.open('rgb_res.jpg')
+im2 = Image.open('hsv_res.jpg')
+im3 = Image.open('hsv_filter.jpg')
+
+combined = Image.new('RGB', (2 * 256, 2 * 256), (250, 250, 250))
+combined.paste(im_orig, (0, 0))
+combined.paste(im1, (256, 0))
+combined.paste(im2, (0, 256))
+combined.paste(im3, (256, 256))
+combined.show()
+combined.save('comb.jpg')
+os.remove('rgb_res.jpg')
+os.remove('hsv_filter.jpg')
+os.remove('hsv_res.jpg')
 cv2.waitKey(0)
-"""
-hsv dlya nalogenia?
-low_black = (15, 80, 40)
-high_black = (20, 205, 155)
-"""
